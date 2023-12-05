3f15ef7b3fc9ce4f39a8fb8d609c8814a267d706
AmelieRacine0 <141154177+AmelieRacine08@users.noreply.github.com>
2023-12-04 21:45:25 -0500

    Remplirles composents

--- src/components/roles/ListeRole.vue
+++ src/components/roles/ListeRole.vue
@@ -1,12 +1,61 @@
 <template>
-    <div>
-        
-    </div>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Prenom</th>
+                <th>Email</th>
+                <th>Date de naissance</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <Etudiant v-for="student in etudiants" :key="student.id" :etudiant="student" @supprimer="supprimer" />
+        </tbody>
+    </table>
+    <button class="btn btn-primary" @click="allerAJouterEtudiant">Ajouter un etudiant</button>
 </template>
 
 <script setup>
     import { ref, reactive } from 'vue';
 
+    const etudiants = ref([])
+import useEtudiant from '../../services/serviceEtudiant.js'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+const { getEtudiants, supprimerEtudiant } = useEtudiant()
+onBeforeMount(() => {
+
+    getEtudiants().then(data => {
+        etudiants.value = data
+
+        console.log('Liste etudiant', data)
+    })
+
+
+})
+import Etudiant from './Etudiant.vue';
+
+const supprimer = (id) => {
+    console.log('emits', id)
+    supprimerEtudiant(id).then((data) => {
+        console.log('suppression', data)
+        getEtudiants().then(data => {
+            etudiants.value = data
+
+            console.log('Liste etudiant', data)
+        }).catch(err => {
+            console.log(err.message)
+        })
+
+    })
+
+}
+
+const allerAJouterEtudiant = () => {
+    router.push('/ajout')
+}
     
 </script>
 
--- src/components/utilisateurs/Utilisateur.vue
+++ src/components/utilisateurs/Utilisateur.vue
@@ -1,15 +1,63 @@
 <template>
-    <div>
-        
-    </div>
+    <tr>
+        <td>
+            {{ utilisateur.prenom }}
+        </td>
+        <td>
+            {{ utilisateur.nom }}
+        </td>
+        <td>
+            {{ utilisateur.email }}
+        </td>
+        <td>
+            {{ utilisateur.dateDeNaissance }}
+        </td>
+        <td><button @click="allerADetail" class="btn btn-primary">Details</button>
+            <button @click="allerAMiseAJour" class="btn btn-success">Editer</button>
+            <button class="btn btn-danger" @click="gererSuppression"> Supprimer</button>
+        </td>
+    </tr>
 </template>
 
 <script setup>
-    import { ref, reactive } from 'vue';
+import { ref, reactive } from 'vue';
 
-    
+//Importer le module qui permet la reidrection vers une autre page
+import { useRouter } from 'vue-router';
+const router=useRouter()
+
+const props = defineProps({
+    etudiant: {
+        type: Object,
+        default: () => ({
+            id: '',
+            nom: 'test',
+            prenom: 'test',
+            email: 'test',
+            dateDeNaissance: 'test'
+        })
+    }
+})
+
+const emit = defineEmits(['supprimer', 'editer'])
+
+const gererSuppression = () => {
+    console.log("Bouton ici")
+    console.log("ID ici", props.etudiant.id)
+    emit('supprimer', props.etudiant.id)
+
+}
+
+// La fonction pour la redirection vers la page Detail
+const allerADetail=()=>{
+    router.push(`/details/${props.etudiant.id}`)
+}
+
+// La fonction pour la redirection vers la page Detail
+const allerAMiseAJour=()=>{
+    router.push(`/mise-a-jour/${props.etudiant.id}`)
+}
+
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
